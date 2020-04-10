3bd64c48e2653cd3bf3f9ab40d9ee3c8a6ac8679
higorasilverio <higorasilverio@outlook.com>
2020-04-10 12:25:37 -0300

    Number: negative or positive - Final

---
+++ src/application/Main.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("*** Number: negative or positive ***");
+		
+		System.out.print("Enter the number: ");
+		int number = scanner.nextInt();
+		if(number < 0) System.out.println("It is negative!");
+		else System.out.println("It is positive!");
+		
+		scanner.close();
+		
+	}
+
+}
