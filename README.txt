aedb49153a5c168b873d73f23a5823b5e9f4e54b
higorasilverio <higorasilverio@outlook.com>
2020-05-13 18:58:25 -0300

    README file

---
+++ README.txt
@@ -0,0 +1,1 @@
+Number: negative or positive?
--- src/application/Main.java
+++ src/application/Main.java
@@ -7,9 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("*** Number: negative or positive ***");
-		
+				
 		System.out.print("Enter the number: ");
 		int number = scanner.nextInt();
 		if(number < 0) System.out.println("It is negative!");
